f7933d6a0dffcbf13d4131a6fdfc001239e2ae69
Josélio <joseliobackup@gmail.com>
2021-12-08 12:12:22 -0300

    teste 2

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 
 
-
-const express = require('express');
+const express = require("express");
+const cors = require('cors');
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const port = 5000;
 
-app.get('/', (req,res) =>{
-    res.json({
-        ok: 'rso'
-    })
-})
-app.get('/nlw1',(req,res)=>{
-    res.json({
-        nlw1: 'oksao'
-    })
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
 })
 
-app.listen(PORT, ()=>{
-    console.log('escutando na porta: ' + PORT);
+app.get('/', (req, res) => {
+    res.json({
+        ok: 'vamos'
+    })
 })
+
+app.get('/nlw', (req, res) => {
+    res.json({
+        ok: 'isso ai'
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is booming on port 5000
+Visit http://localhost:5000`);
+});
