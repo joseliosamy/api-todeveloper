aec5171a754c908c63d62fef2228251aa519d319
Josélio <joseliobackup@gmail.com>
2022-01-01 04:20:50 -0300

    full eps added

--- api/events.js
+++ api/events.js
@@ -38,77 +38,323 @@ module.exports = (req,res) =>{
 				id: 2,
 				eventName: "Next Level Week #2",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw2.jpg?token=ARHRXOJY7BAY4K3ZKL63WITBZNQ2E",
-				redirectURL: "/eventos/next-level-week-2"
+				redirectURL: "/eventos/next-level-week-2",
+				eps: [
+					{
+						epURL: "N_Gp83nwR2w",
+						epName: "Aula 01 - Acelerando sua evolução"
+				},
+					{
+						epURL: "ff2W0baASXA",
+						epName: "Aula 02 - Olhando para as oportunidades"
+				},
+					{
+						epURL: "o8nFAiEM10Q",
+						epName: "Aula 03 - A escolha da stack"
+				},
+					{
+						epURL: "dlkIhtyiJnE",
+						epName: "Aula 04 - Até 2 anos em 2 meses"
+				},
+					{
+						epURL: "kuGEcBZd9D0",
+						epName: "Aula 05 - A milha extra"
+				}
+			]
 			},
 			//NLW3
 			{
 				id: 3,
 				eventName: "Next Level Week #3",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw3.jpg?token=ARHRXOL35JZ5AXVTBKQVTYTBZNQ6E",
-				redirectURL: "/eventos/next-level-week-3"
+				redirectURL: "/eventos/next-level-week-3",
+				eps: [
+					{
+						epURL: "6Ix3uTARak8",
+						epName: "Aula 01 - Acelerando sua evolução"
+				},
+					{
+						epURL: "mdTq8VmL1qI",
+						epName: "Aula 02 - Olhando para as oportunidades"
+				},
+					{
+						epURL: "5wgC4ZuCkv0",
+						epName: "Aula 03 - A escolha da stack"
+				},
+					{
+						epURL: "qF6og0Fbkxc",
+						epName: "Aula 04 - Até 2 anos em 2 meses"
+				},
+					{
+						epURL: "z260PEeDuCc",
+						epName: "Aula 05 - A milha extra"
+				}
+			]
 			},
 			//NLW4 REACT
 			{
 				id: 4,
 				eventName: "Next Level Week #4 - ReactJS",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw4React.jpg?token=ARHRXONRSQUU7QJ2GYTFWZDBZNRB4",
-				redirectURL: "/eventos/next-level-week-4-reactjs"
+				redirectURL: "/eventos/next-level-week-4-reactjs",
+				eps: [
+					{
+						epURL: "Fw38KzGMCiE",
+						epName: "Aula 01 - Rumo ao próximo"
+				},
+					{
+						epURL: "fXT7YMx3gsU",
+						epName: "Aula 02 - Desvendando o Next js"
+				},
+					{
+						epURL: "KboFYC-d50c",
+						epName: "Aula 03 - Contexto e componentes"
+				},
+					{
+						epURL: "2_I_okTLDvc",
+						epName: "Aula 04 - Melhorando a usabilidade da aplicação"
+				},
+					{
+						epURL: "p-3HCTdZy6U",
+						epName: "Aula 05 - Próximo nível com React "
+				}
+			]
 			},
 			//NLW4 NODE
 			{
 				id: 5,
 				eventName: "Next Level Week #4 - Node",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw4Node.jpg?token=ARHRXOLAXIQB53S2MCW763LBZNRAU",
-				redirectURL: "/eventos/next-level-week-4-node"
+				redirectURL: "/eventos/next-level-week-4-node",
+				eps: [
+					{
+						epURL: "0xRj4sk5f58",
+						epName: "Aula 01 - Rumo ao próximo nível"
+				},
+					{
+						epURL: "5vq95MAyeyM",
+						epName: "Aula 02 - Banco de Dados"
+				},
+					{
+						epURL: "PTlQtejf1Qk",
+						epName: "Aula 03 - Testando a nossa aplicação"
+				},
+					{
+						epURL: "uFIMiF9kVak",
+						epName: "Aula 04 - Envio de e mail"
+				},
+					{
+						epURL: "Ge79lY34t1Q",
+						epName: "Aula 05 - Finalizando nossa api com validações"
+				}
+			]
 			},
 			//NLW4 ELIXIR
 			{
 				id: 6,
 				eventName: "Next Level Week #4 - Elixir",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw4Elixir.jpg?token=ARHRXOPV74DUFYVIFJ7RL5LBZNRCA",
-				redirectURL: "/eventos/next-level-week-4-elixir"
+				redirectURL: "/eventos/next-level-week-4-elixir",
+				eps: [
+					{
+						epURL: "gv440V-6Ow8",
+						epName: "Aula 01 - Rumo ao próximo nível"
+				},
+					{
+						epURL: "UZNBKn61pZ4",
+						epName: "Aula 02 - A incrível dupla Phoenix e Ecto"
+				},
+					{
+						epURL: "5CTX24Zq9Cw",
+						epName: "Aula 03 - Indo mais alto"
+				},
+					{
+						epURL: "Uq3sDqrz_48",
+						epName: "Aula 04 - Aprofundando nosso conhecimento"
+				},
+					{
+						epURL: "_e92M-jJG-k",
+						epName: "Aula 05 - Autenticação e Testes"
+				}
+			]
 			},
 			//NLW5 REACT
 			{
 				id: 7,
 				eventName: "Next Level Week #5 - Reactjs",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw5React.jpg?token=ARHRXOJ2BZ37VXM3RKUW5RLBZNRFS",
-				redirectURL: "/eventos/next-level-week-5-reactjs"
+				redirectURL: "/eventos/next-level-week-5-reactjs",
+				eps: [
+					{
+						epURL: "XdHz63rbYBU",
+						epName: "Aula 01 - Litoff "
+				},
+					{
+						epURL: "KUlV2Cg1zQ0",
+						epName: "Aula 02 - Maximum Speed"
+				},
+					{
+						epURL: "aVV_ibUWlFw",
+						epName: "Aula 03 - In Orbit"
+				},
+					{
+						epURL: "Gsr-DKk0NQU",
+						epName: "Aula 04 - Landing"
+				},
+					{
+						epURL: "xsepsbkP61M",
+						epName: "Aula 05 - Surface Exploration"
+				}
+			]
 			},
 			//NLW5 REACT NATIVE
 			{
 				id: 8,
 				eventName: "Next Level Week #5 - React Native",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw5ReactNative.jpg?token=ARHRXOMXUVSQKMFBFGXAJV3BZNRHA",
-				redirectURL: "/eventos/next-level-week-5-react-native"
+				redirectURL: "/eventos/next-level-week-5-react-native",
+				eps: [
+					{
+						epURL: "Y1kFbWH-9CQ",
+						epName: "Aula 01 - Litoff"
+				},
+					{
+						epURL: "lYKBUPiYt_E",
+						epName: "Aula 02 - Maximum Speed"
+				},
+					{
+						epURL: "DANRGC8J1u8",
+						epName: "Aula 03 - In Orbit"
+				},
+					{
+						epURL: "I8l8ZZ7YPbU",
+						epName: "Aula 04 - Landing "
+				},
+					{
+						epURL: "05XnwYKNEtY",
+						epName: "Aula 05 - Surface Exploration "
+				}
+			]
 			},
 			//NLW5 NODE
 			{
 				id: 9,
 				eventName: "Next Level Week #5 - Node",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw5Node.jpg?token=ARHRXOPY6SYS7K5UOMXKGALBZNRFM",
-				redirectURL: "/eventos/next-level-week-5-node"
+				redirectURL: "/eventos/next-level-week-5-node",
+				eps: [
+					{
+						epURL: "khaxJbrVpvk",
+						epName: "Aula 01 - Litoff"
+				},
+					{
+						epURL: "Xz9DKzBBvS0",
+						epName: "Aula 02 - Maximum Speed"
+				},
+					{
+						epURL: "uO6p1SNjwVw",
+						epName: "Aula 03 - In Orbit"
+				},
+					{
+						epURL: "WS9JLf3c50Y",
+						epName: "Aula 04 - Landing "
+				},
+					{
+						epURL: "TzPQH-x7ue4",
+						epName: "Aula 05 - Surface Exploration "
+				}
+			]
 			},
 			//NLW5 ELIXIR
 			{
 				id: 10,
 				eventName: "Next Level Week #5 - Elixir",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw5Elixir.jpg?token=ARHRXOKZC4O2HC2WUGNOB3DBZNRFA",
-				redirectURL: "/eventos/next-level-week-5-elixir"
+				redirectURL: "/eventos/next-level-week-5-elixir",
+				eps: [
+					{
+						epURL: "W4FiLHCr9_M",
+						epName: "Aula 01 - Litoff"
+				},
+					{
+						epURL: "1Ms96-_auyQ",
+						epName: "Aula 02 - Maximum Speed"
+				},
+					{
+						epURL: "DCVtdnA-Tsc",
+						epName: "Aula 03 - In Orbit"
+				},
+					{
+						epURL: "oB2sjcMBkUk",
+						epName: "Aula 04 - Landing"
+				},
+					{
+						epURL: "7sqx6Fj9ejA",
+						epName: "Aula 05 - Surface Exploration"
+				}
+			]
 			},
 			//NLW5 FLUTTER
 			{
 				id: 11,
 				eventName: "Next Level Week #5 - Flutter",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/nlw5Flutter.jpg?token=ARHRXOLEJZ22AJUTB2DXYGLBZNRFG",
-				redirectURL: "/eventos/next-level-week-5-flutter"
+				redirectURL: "/eventos/next-level-week-5-flutter",
+				eps: [
+					{
+						epURL: "IheASU1Ncxg",
+						epName: "Aula 01 - Litoff"
+				},
+					{
+						epURL: "1wDDCkFSQ_I",
+						epName: "Aula 02 - Maximum Speed"
+				},
+					{
+						epURL: "uzQX46aNxIQ",
+						epName: "Aula Bônus"
+				},
+					{
+						epURL: "pvzqLmQYDR4",
+						epName: "Aula 03 - In Orbit"
+				},
+					{
+						epURL: "wHDQGus5IAs",
+						epName: "Aula 04 - Landing "
+				},
+					{
+						epURL: "EvM5CyLR7Mc",
+						epName: "Aula 05 - Surface Exploration "
+				}
+			]
 			},
 			//IRA
 			{
 				id: 12,
 				eventName: "Imersão React Alura",
 				bannerURL: "https://raw.githubusercontent.com/joseliosamy/api-todeveloper/main/images/IRA.jpg?token=ARHRXOJ4OC4QUOS5RNCF6RLBZNRKK",
-				redirectURL: "/eventos/imersao-react-alura"
+				redirectURL: "/eventos/imersao-react-alura",
+				eps: [
+					{
+						epURL: "bPdS3mv3nHo",
+						epName: "Aula 01 - React, Styled Components e Next js"
+				},
+					{
+						epURL: "19rbpDfXROw",
+						epName: "Aula 02 - Linter, Rotas e State"
+				},
+					{
+						epURL: "l5KL1evubew",
+						epName: "Aula 03 - Inputs e página de Quiz"
+				},
+					{
+						epURL: "UBowa7BVu-4",
+						epName: "Aula 04 - Quiz, Pontuação e Integrações com Next js"
+				},
+					{
+						epURL: "KaZz5Z007C0",
+						epName: "Aula 05 - Páginas dinâmicas, animações e Lottie"
+				}
+			]
 			}
 			
 		]);
